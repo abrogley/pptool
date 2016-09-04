00661e2d4da85e300ba55f5d7f3c4b50f2b074a0
abrogley <aaron.c.brogley@nasa.gov>
2016-09-03 22:49:57 -0500

    Adding weighting to random sampling
    Issue: #7

--- main.py
+++ main.py
@@ -2,28 +2,33 @@ exec(open("src/AirportDatabase.py").read())
 exec(open("src/AirportSubset.py").read())
 exec(open("src/AirportManager.py").read())
 exec(open("src/RouteManager.py").read())
+exec(open("src/weightedChoice.py").read())
 
 import random
 
-# Simple tests
+# Setup
 airportDatabase = getAirportList()
 airports = AirportManager(airportDatabase)
 
 myAirportStrings = getMyAirportList()
 myDatabase = []
+myWeightedDatabase = []
 for airportString in myAirportStrings :
-    myDatabase.append(airports.findByName(airportString))
+    currentAirport = airports.findByName(airportString)
+    currentClass = currentAirport.getClass()
+    myDatabase.append(currentAirport)
+    myWeightedDatabase.append([currentAirport, currentClass-.6])
     
 airports = AirportManager(myDatabase)
 router = RouteManager(airports)
 airports.debugPrint = False
 
-# Get two pseudorandom integers from 0-88 inclusive.
+# Get weighted random numbers
 random.seed('We will not surrender our country to the false song of globalism.')
 
 pairs = []
-for ii in xrange(1000) :
-    pairs.append(random.sample(myDatabase, 2))
+for ii in xrange(100) :
+    pairs.append(weightedPair(myWeightedDatabase))
 
 print( "Try to determine the best route between the following city pairs using an upgraded Mohawk." )
 
@@ -48,25 +53,4 @@ for p in pairs:
     print( "        Route efficiency: " + str(routeEfficiency) )
     pairIter += 1
 
-'''
-print( "Try to determine the best route between the following city pairs using an upgraded Aeroeagle." )
-pairs2 = [[32,18], [90,219], [84,204], [156,67]]
-
-for p in pairs2:
-    cumRange = 0
-    ii = 0
-    print( p[0].getCityName() + " and " + p[1].getCityName() )
-    bestRoute = router.findBestRouteBetween(p[0], p[1], 2, 2300)
-    for sr in bestRoute :
-        if ii > 0:
-            cumRange += airports.getDistanceBetween(bestRoute[ii-1], bestRoute[ii])
-        print( "    " + sr.getCityName() )
-        ii += 1
-    perfectRange = airports.getDistanceBetween(bestRoute[0], bestRoute[-1])
-    routeEfficiency = (perfectRange/cumRange)
-    print( "        Range on this route: " + str(cumRange) )
-    print( "        Straight Line Range: " + str(perfectRange) )
-    print( "        Route efficiency: " + str(routeEfficiency) )
-
-'''
 print("Done!")
---
+++ src/weightedChoice.py
@@ -0,0 +1,34 @@
+def weightedChoice(choices):
+    # Sum of all weights
+    total = sum(w for c, w in choices)
+    
+    # Random number up to total
+    r = random.uniform(0, total)
+    
+    # Select random number based on 
+    upto = 0
+    for c, w in choices:
+        if upto + w >= r:
+            return c
+        upto += w
+    assert False, "Shouldn't get here"
+    
+def weightedPair(choicesInput):
+    # Make a local copy of choices
+    choices = list(choicesInput)
+    
+    # Get the first choice
+    first = weightedChoice(choices)
+
+    # Remove first choice from list
+    ii = 0
+    for c, w in choices:
+        if c == first :
+            print("First choice was " + c.getCityName() + ", removing it for Second choice.")
+            choices.pop(ii)
+        ii += 1
+    print("New array length: " + str(len(choices)))
+    # Select a new choice
+    second = weightedChoice(choices)
+    print("   Second choice was " + second.getCityName())
+    return [first, second]
